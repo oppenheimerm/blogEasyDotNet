539d9cea9bd123198e8e0b7453f3f78e7e323783
michieloppenheimer <michieloppenheimer@gmail.com>
2023-01-20 17:00:25 +0000

    add core entities

---
+++ BE.Core/PostTag.cs
@@ -0,0 +1,23 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace BE.Core
+{
+    public class PostTag
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Post")]
+        public int PostId { get; set; }
+
+        public string? TagNameEncoded { get; set; }
+
+        [DataMember]
+        public string? TagName { get; set; }
+
+        public Post? Post { get; set; }
+    }
+}

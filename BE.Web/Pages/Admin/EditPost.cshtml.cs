d5e5da6eb727e71549cc7d4e23185e2e0b9234f1
michieloppenheimer <michieloppenheimer@gmail.com>
2023-01-26 20:30:22 +0000

    admin pages added

---
+++ BE.Web/Pages/Admin/EditPost.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BE.Web.Pages.Admin
+{
+    public class EditPostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BE.Web/Pages/Admin/Logout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BE.Web.Pages.Admin
+{
+    public class LogoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
